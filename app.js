44e7db895096a7b32dd015cd47b134850686be7b
Leonardo G <dgarzonf@unal.edu.co>
2023-01-24 15:47:57 -0500

    Cambios para que funcione en mogno

--- app.js
+++ app.js
@@ -7,76 +7,107 @@ const {
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/mock')
-const delay = (ms) => new Promise((res) => setTimeout(res, ms))
+const MongoAdapter = require('@bot-whatsapp/database/mongo')
 
-/***
-* Simular peticion async http 1.5 segundos
+/**
+* Declaramos las conexiones de Mongo
 */
-const fakeHTTPMenu = async () => {
-  console.log('⚡ Server request!')
-  await delay(1500)
-  console.log('⚡ Server return!')
-  return Promise.resolve([{ body: 'Arepas' }, { body: 'Empanadas' }])
-}
 
-/***
-* Simular peticion async http 0.5 segundos
+const MONGO_DB_URI = 'mongodb://0.0.0.0:27017'
+const MONGO_DB_NAME = 'db_bot'
+
+/**
+* Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
+*
+*          Menu Principal
+*           - SubMenu 1
+*             - Submenu 1.1
+*           - Submenu 2
+*             - Submenu 2.1
+*
+* Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
 */
-const fakeHTTPPayment = async () => {
-  const link = `https://www.buymeacoffee.com/leifermendez?t=${Date.now()}`
-  console.log('⚡ Server request!')
-  await delay(500)
-  console.log('⚡ Server return!')
-  return Promise.resolve([
-      { body: `Puedes hacer un *pago* en el siguiente link: ${link}` },
-  ])
-}
 
-const flujoCash = addKeyword('efectivo').addAnswer(
-  'Ok te espero con los billetes'
-)
-const flujosOnline = addKeyword('online').addAnswer(
-  ['Te envio el link'],
-  null,
-  async (_, { flowDynamic }) => {
-      const link = await fakeHTTPPayment()
-      return flowDynamic(link)
-  }
-)
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer([
+  '📄 Aquí tenemos el flujo secundario',
+])
 
-const flujoPedido = addKeyword(['pedido', 'pedir']).addAnswer(
-  '¿Como quieres pagar en *efectivo* o *online*?',
+const flowDocs = addKeyword([
+  'doc',
+  'documentacion',
+  'documentación',
+]).addAnswer(
+  [
+      '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+      'https://bot-whatsapp.netlify.app/',
+      '\n*2* Para siguiente paso.',
+  ],
   null,
   null,
-  [flujoCash, flujosOnline]
+  [flowSecundario]
 )
 
-const conversacionPrincipal = addKeyword(['hola', 'ole', 'buenas'])
-  .addAnswer('Bienvenido al restaurante *La cuchara de palo 🙌*')
+const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+  [
+      '🙌 Aquí encontras un ejemplo rapido',
+      'https://bot-whatsapp.netlify.app/docs/example/',
+      '\n*2* Para siguiente paso.',
+  ],
+  null,
+  null,
+  [flowSecundario]
+)
+
+const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+  [
+      '🚀 Puedes aportar tu granito de arena a este proyecto',
+      '[*opencollective*] https://opencollective.com/bot-whatsapp',
+      '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+      '[*patreon*] https://www.patreon.com/leifermendez',
+      '\n*2* Para siguiente paso.',
+  ],
+  null,
+  null,
+  [flowSecundario]
+)
+
+const flowDiscord = addKeyword(['discord']).addAnswer(
+  [
+      '🤪 Únete al discord',
+      'https://link.codigoencasa.com/DISCORD',
+      '\n*2* Para siguiente paso.',
+  ],
+  null,
+  null,
+  [flowSecundario]
+)
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+  .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
   .addAnswer(
-      `El menu del día es el siguiente`,
+      [
+          'te comparto los siguientes links de interes sobre el proyecto',
+          '👉 *doc* para ver la documentación',
+          '👉 *gracias*  para ver la lista de videos',
+          '👉 *discord* unirte al discord',
+      ],
       null,
-      async (_, { flowDynamic }) => {
-          const menu = await fakeHTTPMenu()
-          return flowDynamic(menu)
-      }
+      null,
+      [flowDocs, flowGracias, flowTuto, flowDiscord]
   )
-  .addAnswer('👉 Si deseas ordenar escribe *pedir*', { delay: 1500 }, null, [
-      flujoPedido,
-  ])
 
 const main = async () => {
-  const adapterDB = new MockAdapter()
-  const adapterFlow = createFlow([conversacionPrincipal])
+  const adapterDB = new MongoAdapter({
+      dbUri: MONGO_DB_URI,
+      dbName: MONGO_DB_NAME,
+  })
+  const adapterFlow = createFlow([flowPrincipal])
   const adapterProvider = createProvider(BaileysProvider)
-
   createBot({
       flow: adapterFlow,
       provider: adapterProvider,
       database: adapterDB,
   })
-
   QRPortalWeb()
 }
 
